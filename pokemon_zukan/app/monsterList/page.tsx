cdcefe61c7f78012e5c9f4f254394c642493f43b
hukuryo <ryohei.1998.05.09@gmail.com>
2023-11-25 21:38:58 +0900

    一覧ページを作成

--- pokemon_zukan/app/monsterList/page.tsx
+++ pokemon_zukan/app/monsterList/page.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-export default function () {
+export default function monsterList() {
+  const pokemonList = [
+    // ここにポケモンのデータが入る想定
+    { id: 1, name: "ピカチュウ" },
+    { id: 2, name: "ゼニガメ" },
+    // 他のポケモンデータも追加
+  ];
+
   return (
-    <div>
-      <h1>モンスター一覧画面</h1>
+    <div className="container mx-auto my-8">
+      <h1 className="text-3xl font-bold mb-6">モンスター一覧画面</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {pokemonList.map((pokemon) => (
+          <div
+            key={pokemon.id}
+            className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 ease-in-out"
+          >
+            <h2 className="text-xl font-bold mb-2">{pokemon.name}</h2>
+            {/* 他のポケモン情報を表示 */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
