d52bf7450279f33a4278d6b3ba13be1e71d43dd4
hukuryo <ryohei.1998.05.09@gmail.com>
2023-11-26 22:39:53 +0900

    ✨  NotFoundページとローディングページを追加

--- pokemon_zukan/app/not-found.tsx
+++ pokemon_zukan/app/not-found.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function notFound() {
-  return <div>not-found</div>;
-}
+const NotFound = () => {
+  return (
+    <div className="container mx-auto my-8 text-center">
+      <h1 className="text-4xl font-bold mb-4">404 Not Found</h1>
+      <p className="text-gray-600">お探しのページが見つかりませんでした。</p>
+    </div>
+  );
+};
+
+export default NotFound;
