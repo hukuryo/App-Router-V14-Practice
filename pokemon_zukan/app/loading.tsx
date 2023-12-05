a92e0a38cbd60e0ed3e8cbf7df2d0f50a48300f0
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-05 23:01:36 +0900

    spinnerを表示

--- pokemon_zukan/app/loading.tsx
+++ pokemon_zukan/app/loading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { Spinner } from "./components/spinner";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full border-t-4 border-blue-500 border-solid h-16 w-16"></div>
-      <div className="ml-4 text-xl font-semibold text-gray-800">Loading...</div>
+    <div>
+      <Spinner />
     </div>
   );
 };
