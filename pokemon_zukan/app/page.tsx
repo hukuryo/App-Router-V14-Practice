dae12e17b6b40793d2dadb645666aeb910a25f0b
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-09 21:07:36 +0900

    ホーム画面を調整

--- pokemon_zukan/app/page.tsx
+++ pokemon_zukan/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Spinner } from "./components/spinner";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Spinner />
+      <h1>ホーム画面</h1>
+      <Link href="/monsterList" legacyBehavior>
+        <a className="cursor-pointer">モンスター一覧画面へ</a>
+      </Link>
     </main>
   );
 }
