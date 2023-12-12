5fc19971311b2d02ab0b21f38ebf3734237aad4c
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-12 22:28:31 +0900

    詳細ページの実装をしてみた

--- pokemon_zukan/app/monsterList/[monsterId]/page.tsx
+++ pokemon_zukan/app/monsterList/[monsterId]/page.tsx
@@ -1,33 +1,39 @@
 import Link from "next/link";
+import { notFound } from "next/navigation";
 import React from "react";
 
 type detailType = {
-  params: string;
+  params: {
+    monsterId: number
+  }
 };
 
 type pageProps = {
-  params: {
-    id: number;
-    title: string;
-    body: string;
-  };
+  id: number;
+  title: string;
+  body: string;
 };
 
 async function fetchItemDetail(id: number) {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  // const monsterDetail: pageProps = res.json();
-  return res.json();
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+  const monsterDetail: pageProps = await res.json();
+  return monsterDetail
 }
 
-export default function MonsterDetailPage({ params }: detailType) {
-  // const monster = fetchItemDetail(params);
-  // console.log(monster);
+export default async function page({ params }: detailType) {
+  const monster = await fetchItemDetail(params.monsterId)
+  if(!monster) return notFound()
   return (
     <div className="container mx-auto my-8">
       <div className="bg-white p-8 rounded-lg shadow-md">
-        {/* <div className="text-center mb-6">{monster}</div> */}
+        <h2 className="font-bold text-center mb-2">Title</h2>
+        <div className="text-center mb-6">{monster.title}</div>
+        <h2 className="font-bold text-center mb-2">body</h2>
+        <div className="text-center mb-6">{monster.body}</div>
         <div className="flex justify-center">
-          <Link href={"/monsterList"}>リストに戻る</Link>
+          <Link href={"/monsterList"} className="hover:underline">リストに戻る</Link>
         </div>
       </div>
     </div>
