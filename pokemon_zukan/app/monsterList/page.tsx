eb581526c8a227a26e2024bb868ce0980b7e4ad0
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-13 22:49:46 +0900

    Hydrationできるようにした

--- pokemon_zukan/app/monsterList/page.tsx
+++ pokemon_zukan/app/monsterList/page.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import { PokemonList } from "../components/pokemonList";
+import Loading from "../loading";
 
 const MonsterList = () => {
   return (
     <div className="container mx-auto my-8">
       <h1 className="text-3xl font-bold mb-6">モンスター一覧画面</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        <PokemonList />
+        <Suspense fallback={<Loading />}>
+          <PokemonList />
+        </Suspense>
       </div>
     </div>
   );
